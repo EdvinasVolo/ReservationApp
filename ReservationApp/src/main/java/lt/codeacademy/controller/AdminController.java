8e5c055788a815a2f6653112c01d051a7d4d7e16
Paulina <paulina.matulionyte@gmail.com>
2022-08-23 10:24:38 +0300

    Security turn off

--- ReservationApp/src/main/java/lt/codeacademy/controller/AdminController.java
+++ ReservationApp/src/main/java/lt/codeacademy/controller/AdminController.java
@@ -1,5 +1,31 @@
 package lt.codeacademy.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lt.codeacademy.service.ClientService;
+
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
 
+	@Autowired
+	private ClientService clientService; 
+	
+	@GetMapping("/admin")
+	public String sayHello() {
+		return "Admin: Viskas OK";
+	}	
+	
+//	
+//	@GetMapping("/index")
+//	public String showUserList(Model model) {
+//	    model.addAttribute("clients", clientService.findAll());
+//	    return "admin/index";
+//	}
+	
 }
