a1fe9336e06cce1c87f3808dbbb2eb24d62b686d
Rytis <rytutukas123@gmail.com>
2022-09-12 16:03:10 +0300

    hhhhhhhhhhhhhhhhhhhhhhhhh

--- ReservationApp/src/main/java/lt/codeacademy/controller/LoginController.java
+++ ReservationApp/src/main/java/lt/codeacademy/controller/LoginController.java
@@ -1,6 +1,12 @@
 package lt.codeacademy.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,8 +16,23 @@ public class LoginController {
 	
 	 @GetMapping("/login")
 	    public String redirectTo(){
-	        return "/index/login.html";
+	        return "index/login";
 	    }
-	
+
+
+		@GetMapping("/login?error")
+		public String login(HttpServletRequest request, Model model) {
+			HttpSession session = request.getSession(false);
+			String errorMessage = null;
+			if (session != null) {
+				AuthenticationException ex = (AuthenticationException) session
+						.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+				if (ex != null) {
+					errorMessage = ex.getMessage();
+				}
+			}
+	model.addAttribute("errorMessage", errorMessage);
+			return "index/login";
+		}
 
 }
--- ReservationApp/src/main/java/lt/codeacademy/security/SecurityConfig.java
+++ ReservationApp/src/main/java/lt/codeacademy/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package lt.codeacademy.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,47 +18,50 @@ import lt.codeacademy.service.UserAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
-   
-		@Bean
-	    public BCryptPasswordEncoder passwordEncoder() {
-	        return new BCryptPasswordEncoder();
-	    }
-
-    
-    @Autowired
-    private UserAuthenticationSuccessHandler successHandler;
-    
-
-
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-    	http
-    	.cors().disable()
-    	.csrf().disable()
-        .authorizeRequests() 
-        .antMatchers("/admin/**").hasAnyAuthority("ADMIN")
-        .antMatchers("/service").permitAll()
-       // .antMatchers("/service/**").hasAnyAuthority("ROLE_ADMIN")
-        .anyRequest().permitAll()
-        .and()
-        .formLogin()
-        .permitAll()
-        .successHandler(successHandler)
-        .loginPage("/login")
-        .permitAll()
-        .loginProcessingUrl("/process-login")
-        .permitAll()
-        .defaultSuccessUrl("/service", true)
-        .permitAll()
-        .and()
-        .logout().logoutSuccessUrl("/?logout=true")
-        .invalidateHttpSession(true)
-        .deleteCookies("JSESSIONID").permitAll()
-      ;
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
+    @Autowired
+    private DataSource dataSource;
+
+    @Autowired
+    private UserAuthenticationSuccessHandler successHandler;
+    @Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)
+	  throws Exception {
+		 auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+	      .dataSource(dataSource)
+	      .usersByUsernameQuery("select username, password, enabled from users where username=?")
+	        .authoritiesByUsernameQuery("select username, role from users where username=?");
+	}
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+                // .cors().disable()
+                // .csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/service/**").hasAnyAuthority("ROLE_ADMIN","ROLE_USER")
+                //.antMatchers("/service").permitAll()
+                // .antMatchers("/service/**").hasAnyAuthority("ROLE_ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .successHandler(successHandler)
+                .loginPage("/login")
+                
+                .loginProcessingUrl("/process-login")
+                .permitAll()
+                .defaultSuccessUrl("/service", true)
+                .permitAll()
+                .failureUrl("/login?error")
+                .and()
+                .logout().logoutSuccessUrl("/?logout=true")
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID").permitAll();
+    }
 
 }
